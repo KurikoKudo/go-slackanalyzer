5c1df62a98da1b8ad6228fd36ef4ebc26cff6f01
Kuriko <e155407w@st.u-gakugei.ac.jp>
2018-04-26 20:47:57 +0900

    fix walkfunk

--- DirectoryLoader.go
+++ DirectoryLoader.go
@@ -8,29 +8,37 @@ import (
 var messageList []Message
 
 //DirectoryLoader is loading directory function
-func DirectoryLoader (dirPath string) error{
+func DirectoryLoader (rootPath string) error{
 
-	err := filepath.Walk(dirPath, func (path string, info os.FileInfo, err error) error {
-
-		if !info.IsDir(){
-			fileloader(path)
-		}
-
-
-		return nil
-	})
-
+	err := filepath.Walk(rootPath, walkFunc)
+	
 	return err
 }
 
-func fileloader(path string){
+func walkFunc(filePath string, info os.FileInfo, err error) error {
+
+	if !info.IsDir(){
+		message := fileLoader(filePath)
+		messageList = append(messageList,message)
+	}
+
+	return nil
+}
+
+func fileLoader(filePath string) Message {
+
+	var message Message
 
 	//pathのjsonファイルをopenする
+
+	//とりあえず
+	_ = filePath
 
 	//読み込みにはjsonのパッケージ使えそう
 
 	//Messageに値をセットする
 
 	//appendする
-	
+
+	return message
 }
